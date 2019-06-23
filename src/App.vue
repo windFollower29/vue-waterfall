74c595ab7cec8093a5bd98c38c6e0e713b32982f
windFollower29 <125479080@qq.com>
2019-06-24 00:16:58 +0800

    second page fail

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <waterfall
+      :list="list"
+      @nomore="nomore"
+    />
+  </div>
+</template>
+
+<script>
+
+import Waterfall from './components/Waterfall'
+
+import axios from 'axios'
+
+export default {
+
+  components: {
+    Waterfall
+  },
+
+  data () {
+    return {
+      list: []
+    }
+  },
+
+  created () {
+
+    this.getList()
+
+    window.addEventListener('scroll', e => {
+
+      this.onScroll()
+    }, false)
+
+
+  },
+
+  methods: {
+
+    onScroll () {
+
+    },
+
+    getList () {
+
+      axios.get('../static/mock/data.json?group=' + this.group++)
+      .then(res => {
+        console.log(res)
+        this.list = res.data
+      })
+    },
+
+    nomore () {
+      this.getList()
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
